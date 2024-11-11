8c9256ae2b5eff5b9966818bad62a5c503508320
Abhay Gupta <ag4035737@gmail.com>
2024-11-11 08:57:09 -0800

    Create A2prj.8.c

---
+++ assignment_2project/A2prj.8.c
@@ -0,0 +1,12 @@
+//print the series apk bql crm eto.
+#include <stdio.h>
+
+int main() {
+    int i,j;
+    printf("enter the term:");
+    scanf("%d",&j);
+    for(i=0; i<j; i++){
+       printf("%c%c%c\t",'A'+i,'P'+i,'K'+i);
+    }
+    return 0;
+}
