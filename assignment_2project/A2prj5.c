0be04248786e90f09ec7849906a6970b057576ea
Abhay Gupta <ag4035737@gmail.com>
2024-11-11 04:12:10 -0800

    Create A2prj5.c

---
+++ assignment_2project/A2prj5.c
@@ -0,0 +1,14 @@
+//7 10 8 11
+#include<stdio.h>
+int main(){
+    int n,i;
+    printf("enter the termes:");
+    scanf("%d",&n);
+    for(i=0; i<n; i++){
+        if(i%2==0)
+        printf("%d\t",7+(i/2));
+        else
+        printf("%d\t",10+(i/2));
+    }
+    return 0;
+}
