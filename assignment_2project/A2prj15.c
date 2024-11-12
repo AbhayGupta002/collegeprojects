584a416245ec6e026dd464ec78b1056df10b536d
Abhay Gupta <ag4035737@gmail.com>
2024-11-11 23:51:02 -0800

    Create A2prj15.c

---
+++ assignment_2project/A2prj15.c
@@ -0,0 +1,23 @@
+//print ASCII charcter:
+#include <stdio.h>
+
+int main() {
+
+    int i = 32; // Start from ASCII value 32 (space character)
+    char ch;
+
+    do {
+        // Print the ASCII value and corresponding character
+        printf("ASCII value is %d character: '%c'\n", i, i);
+        
+        // Ask user if they want to continue
+        printf("Do you want to see the next character? (y/n): ");
+        scanf(" %c", &ch); // Notice the space before %c to consume any leftover newline
+
+        i++; // Increment the ASCII value
+    } while(ch == 'y' || ch == 'Y');
+
+    printf("Program terminated\n");
+
+    return 0;
+}
